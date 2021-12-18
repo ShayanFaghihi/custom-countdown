10d0d9d0b0f9a6fab93560a84bc88226c7f38b0e
unknown <shayan.faghihi93@gmail.com>
2021-12-18 10:00:23 +0330

    Changing the whole process based on the video I've learnt

--- script.js
+++ script.js
@@ -1,38 +1,41 @@
-// Form Page
-const formPage = document.querySelector(".form-page");
+// Input Container
+const inputPage = document.querySelector(".form-page");
+const inputForm = document.getElementById("inputForm");
+const dateInput = document.getElementById("date");
 const submitBtn = document.getElementById("submit");
 
+let inputTitle = '';
+let inputDate = '';
 
-// Timer Page
-const timerPage = document.querySelector('.timer-page');
-const timerPageData = document.querySelector(".timer-page .timer");
 
+// Timer Container
+const timerPage = document.querySelector(".timer-page");
+const timerTitle = document.getElementById("timerTitle");
+
+// Today in standar format (YY-MM-DD)
+const today = new Date().toISOString().split('T')[0];
+// Set a minimum attribute for date input based on today date
+dateInput.setAttribute("min",today);
+
+
+
+
+// Get Input Form Data
+const getFormData = (e) => {
+    e.preventDefault();
+     inputTitle = e.srcElement[0].value;
+     inputDate = e.srcElement[1].value;
+
+     showTimer();
+}
+
+
+const showTimer = () => {
+    inputPage.hidden = true;
+    timerPage.hidden = false;
+    timerTitle.textContent = inputTitle;
+}
 
 
 // Event Listeners
-submitBtn.addEventListener("click", () => {
-    const countdownTitle = document.getElementById("title").value;
-    const countdonwDate = document.getElementById("date").value;
-    const final = new Date(countdonwDate).getTime();
-    const now = new Date().getTime();
-
-    // Calvulating amount of days/hours/minutes/second left to the due time
-    const day = Math.ceil(((((final - now) / 1000) / 60) / 60 ) / 24);
-    const hour = Math.ceil(((((final - now) / 1000) / 60) / 60 ));
-    const minute = Math.ceil((((final - now) / 1000) / 60));
-    const second = Math.ceil(((final - now) / 1000));
-
-    showTimer(day,hour,minute,second);
-})
-
-
-function showTimer(day,hour,minute,second) {
-    formPage.hidden = true;
-    timerPage.hidden = false;
-    timerPageData.innerHTML = `
-        <div class="timer__title">${day}<span class="timer__tab">Days</span></div>
-        <div class="timer__title">${hour}<span class="timer__tab">Hours</span></div>
-        <div class="timer__title">${minute}<span class="timer__tab">Minutes</span></div>
-        <div class="timer__title">${second}<span class="timer__tab">Seconds</span></div>
-    `
-}
+inputForm.addEventListener('submit',getFormData);
